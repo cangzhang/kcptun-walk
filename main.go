dc132ddff1d6ec4ba06cec50174977918ba36668
al <cangzan@gmail.com>
2021-09-08 14:28:47 +0800

    feat: tray.

--- main.go
+++ main.go
@@ -27,11 +27,16 @@ func (config *Config) logToTextarea(text string) {
 
 func main() {
 	var config Config
+	mainW, err := walk.NewMainWindow()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	if _, err := (MainWindow{
-		Title:  "Kcptun Walk",
-		Size:   Size{300, 500},
-		Layout: VBox{},
+	mainWConfig := MainWindow{
+		AssignTo: &mainW,
+		Title:    "Kcptun Walk",
+		Size:     Size{300, 500},
+		Layout:   VBox{},
 		Children: []Widget{
 			PushButton{
 				Text: "Run",
@@ -57,10 +62,62 @@ func main() {
 			TextEdit{
 				AssignTo: &config.textEdit,
 				ReadOnly: true,
-				VScroll: true,
+				VScroll:  true,
 			},
 		},
-	}).Run(); err != nil {
+	}
+
+	icon, err := walk.Resources.Icon("./assets/icon.ico")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ni, err := walk.NewNotifyIcon(mainW)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ni.Dispose()
+
+	if err := ni.SetIcon(icon); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := ni.SetToolTip("Click for info or use the context menu to exit."); err != nil {
+		log.Fatal(err)
+	}
+
+	// When the left mouse button is pressed, bring up our balloon.
+	ni.MouseDown().Attach(func(x, y int, button walk.MouseButton) {
+		if button != walk.LeftButton {
+			return
+		}
+
+		if err := ni.ShowCustom(
+			"Walk NotifyIcon Example",
+			"There are multiple ShowX methods sporting different icons.",
+			icon); err != nil {
+
+			log.Fatal(err)
+		}
+	})
+
+	// We put an exit action into the context menu.
+	exitAction := walk.NewAction()
+	if err := exitAction.SetText("E&xit"); err != nil {
+		log.Fatal(err)
+	}
+	exitAction.Triggered().Attach(func() { walk.App().Exit(0) })
+	if err := ni.ContextMenu().Actions().Add(exitAction); err != nil {
+		log.Fatal(err)
+	}
+	if err := ni.SetVisible(true); err != nil {
+		log.Fatal(err)
+	}
+	// Now that the icon is visible, we can bring up an info balloon.
+	if err := ni.ShowInfo("Walk NotifyIcon Example", "Click the icon to show again."); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := mainWConfig.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
